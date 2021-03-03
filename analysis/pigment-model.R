ca94fac93bd9e0169bd271f725cb651a3643c113
Stefano Mezzini <stefano@menouno.com>
2021-03-03 11:25:40 -0600

    pigment Tweedie location-scale-shape model

---
+++ analysis/pigment-model.R
@@ -0,0 +1,46 @@
+library('mgcv')
+library('scam')
+library('ggplot2')
+library('readxl')
+library('tidyr')
+library('cowplot')
+library('dplyr')
+library('gratia')
+theme_set(theme_bw())
+NCORES <- 4 # number of logical processors/cores
+
+# import data
+pigments <- readRDS('data/kwb-data.rds')
+
+# pigment overview (different trends, different smoothness)
+ggplot(pigments, aes(mid.depth, conc)) +
+  facet_grid(pigment ~ lake, scales = 'free_y', labeller = label_parsed) +
+  geom_point(aes(size = weight), alpha = 0.3) +
+  geom_smooth(se = FALSE, method = 'gam', formula = y ~ s(x, k = 30)) +
+  scale_x_reverse() +
+  scale_size('Weight', range = c(0.75, 1.25)) +
+  labs(x = 'Depth (cm, reversed)', y = 'Concentration')
+
+# modelling ####
+# by = pigment?
+m.twlss <- gam(list(conc ~ s(year.scam, pigment_lake, k = 30, bs = 'fs'),
+                    # constant mean-variance relationship
+                    ~ 1,
+                    # scale varies over time and by amount of info/slice
+                    ~ s(year.scam, pigment_lake, k = 10, bs = 'fs') +
+                      s(interval, k = 4, bs = 'cr')), # 3 gives \, 5 gives w
+               data = pigments,
+               family = twlss(),
+               weights = weight,
+               method = 'REML',
+               control = gam.control(nthreads = NCORES))
+
+appraise(m.twlss)
+
+summary(m.twlss)
+draw(m.twlss)
+
+# residuals
+pigments$e <- pigments$conc - fitted(m.twlss)[,1]
+
+# derivatives?
