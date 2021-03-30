3e70de4d1a435059bd5141e95c1859c15ed745d2
Stefano Mezzini <stefano@menouno.com>
2021-03-30 15:25:13 -0600

    changed axis to estimated year

--- analysis/isotopes.R
+++ analysis/isotopes.R
@@ -7,6 +7,7 @@ source('analysis/default-figure-styling.R')
 # import data
 isotopes <-
   readRDS('data/isotopes.rds') %>%
+  filter(year.scam > 1800) %>%
   pivot_longer(d15N:percC, names_to = 'name', values_to = 'value') %>%
   mutate(lab = case_when(name == 'd15N' ~ 'delta^{15}~N~\'\211\'',
                          name == 'percN' ~ 'N~\'\045\'',
@@ -18,7 +19,7 @@ isotopes <-
 p.isotopes <-
   ggplot(isotopes) +
   facet_grid(lab ~ lake, scales = 'free_y', labeller = label_parsed, switch = 'y') +
-  geom_point(aes(mid.depth, value), alpha = 0.5) +
+  geom_point(aes(year.scam, value), alpha = 0.5) +
   labs(x = 'Year C.E.', y = NULL) +
   theme(strip.background = element_blank(), strip.placement = 'outside')
 #p2pdf('isotopes.pdf', p.isotopes, x.plots = 1.5, y.plots = 2, device = 'pdf')
